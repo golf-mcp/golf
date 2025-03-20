2db21ad5f11c9f27bf1784fe75f114ce874fb104
aschlean <antoni@gmitruk.com>
2025-03-20 14:11:37 +0100

    fix[bridge] works!

--- demo/client/bridge/op_bridge.py
+++ demo/client/bridge/op_bridge.py
@@ -18,7 +18,9 @@ else:
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-from ..op_client_authed import OnePasswordAuthedClient
+# Add parent directory to path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from op_client_authed import OnePasswordAuthedClient
 
 logger.info("Initializing MCP bridge server...")
 
