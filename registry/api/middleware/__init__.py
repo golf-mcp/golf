a090af8bfb81a6408ee9d3fdbac341632ae6fd61
aschlean <antoni@gmitruk.com>
2025-03-21 15:37:09 +0100

    fix[core] init

--- registry/api/middleware/__init__.py
+++ registry/api/middleware/__init__.py
@@ -1,9 +1,11 @@
 from .rate_limit import RateLimitMiddleware
 from .security import SecurityMiddleware
 from .cors import CORSMiddleware
+from .auth import AuthMiddleware
 
 __all__ = [
     'RateLimitMiddleware',
     'SecurityMiddleware',
-    'CORSMiddleware'
+    'CORSMiddleware',
+    'AuthMiddleware'
 ] 
